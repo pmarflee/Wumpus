b317e668969dfef455d9a0843558d368d5c907af
Paul Marfleet <paul.marfleet@gmail.com>
2014-12-13 23:08:16 +0000

    Asserted that room links are bidirectional.  Added VS11 support for running tests.

--- Wumpus.Tests/CaveFeatureSteps.fs
+++ Wumpus.Tests/CaveFeatureSteps.fs
@@ -8,11 +8,17 @@ open Wumpus.Core.Model
 
 let mutable Number : int = 0
 
+let parseExits (exits : string) = exits.Split(',') |> Array.map (fun n -> Int32.Parse(n)) |> Array.toList 
+
 let [<Given>] ``the standard Wumpus cave system`` () = ()
 
 let [<When>] ``I am in room (.*)`` (number : int) =
     Number <- number
 
 let [<Then>] ``I should see exits (.*)`` (exits : string) =
-    let expected = exits.Split(',') |> Array.map (fun n -> Int32.Parse(n)) |> Array.toList 
+    let expected = parseExits exits
     Cave.Rooms.[Number].Exits |> should equal expected
+
+let [<Then>] ``I should be able to enter and return from all exits`` () =
+    for exit in Cave.Rooms.[Number].Exits do
+        Cave.Rooms.[exit].Exits |> should contain Number
